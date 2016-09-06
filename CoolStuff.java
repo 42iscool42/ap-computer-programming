1e29c0b77ad334f1c6b454f0348b629a5cd4417c
Nate The Plate <42iscool42@gmail.com>
2016-09-06 12:34:04 +0000

    First

---
+++ CoolStuff.java
@@ -0,0 +1,7 @@
+public class CoolStuff {
+    public static void main(String[] args) {
+        for (int i = 0; i<6; i++) {
+            System.out.println("Grant is also a nyerd");
+        }
+    }
+}
